5954dfd74fa99eb6e53de3959e2724d9cf49f0da
Lawand Othman <lwnd@pm.me>
2022-11-09 06:01:38 +0000

    remove expressions

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,15 +1,11 @@
 pub enum Node {
     Program(Program),
     Statement(Statement),
-    Expression(Expression),
 }
 #[derive(Debug, PartialEq, Eq)]
 pub enum Statement {
     Let(String),
     Return,
-}
-pub enum Expression {
-    Identifier(String),
 }
 pub struct Program {
     pub statements: Vec<Statement>,
